8c410e9e9031460be5b1b1b07829e8e9a38f82bb
Kaleb98 <69730982+Kaleb98@users.noreply.github.com>
2026-02-23 13:45:16 -0500

    Change response content type and modify feed URL

--- api/fetch-jobs.js
+++ api/fetch-jobs.js
@@ -2,12 +2,10 @@ const https = require("https");
 
 module.exports = async function (req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Content-Type", "text/plain");
 
-  const url = "https://careers.lilly.com/us/en/search-results?format=json";
-
-  // Try their job feed via the refNum we found (LILLUS)
-  const feedUrl = "https://content-us.phenompeople.com/api/content-delivery/caasContentV1?refNum=LILLUS&lang=en_us&blob=jobwidgetsettings";
+  // Using exact params the API told us it needs
+  const feedUrl = "https://content-us.phenompeople.com/api/content-delivery/caasContentV1?refNum=LILLUS&siteType=external&locale=en_us&pageId=page17&blob=jobwidgetsettings";
 
   try {
     const data = await new Promise((resolve, reject) => {
@@ -15,6 +13,7 @@ module.exports = async function (req, res) {
         headers: {
           "User-Agent": "Mozilla/5.0",
           "Accept": "application/json",
+          "Referer": "https://careers.lilly.com/us/en/search-results"
         }
       }, (r) => {
         let body = "";
@@ -23,7 +22,7 @@ module.exports = async function (req, res) {
       }).on("error", reject);
     });
 
-    res.send(data || JSON.stringify({ jobs: [] }));
+    res.send(data || "empty response");
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
